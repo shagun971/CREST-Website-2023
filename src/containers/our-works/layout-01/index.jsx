935fbcc544e422a19cca7a4c5ac71e808c13acfa
Keith.M <keithmoc.dev@gmail.com>
2022-11-18 11:35:34 +1030

    updated: our works section to highlight our current projects and research

--- src/containers/our-works/layout-01/index.jsx
+++ src/containers/our-works/layout-01/index.jsx
@@ -2,25 +2,26 @@ import BoxLargeImage from "@components/box-large-image/layout-01";
 import CustomSectionTitle from "@ui/custom-section-title";
 import { Col, Container, Row } from "@ui/wrapper";
 import React from "react";
-import { data } from "./../data";
 import { SectionWrap } from "./style";
+import { ItemType } from "@utils/types";
+import PropTypes from "prop-types";
 
-const OurWorkSection = () => {
+const OurWorkSection = ({ data }) => {
     return (
         <SectionWrap>
             <Container>
                 <Row>
                     <Col xl={12}>
                         <CustomSectionTitle
-                            subtitle={data.subtitle}
-                            title={data.title}
-                            description={data.description}
+                            subtitle={data.section_title.subtitle}
+                            title={data.section_title.title}
+                            description={data.section_title.description}
                         />
 
                     </Col>
                 </Row>
                 <Row className="gutter-xl-70 vAlign">
-                    {data.clusters.map((item) => {
+                    {data.items.map((item) => {
                         return (
                             <Col
                                 lg={4}
@@ -30,7 +31,7 @@ const OurWorkSection = () => {
                             >
                                 <BoxLargeImage
                                     title={item.title}
-                                    image={item.image}
+                                    image={item.images?.[0]}
                                     path={item.path}
                                 />
                             </Col>
@@ -40,6 +41,12 @@ const OurWorkSection = () => {
             </Container>
         </SectionWrap>
     );
+};
+
+OurWorkSection.propTypes = {
+    data: PropTypes.shape({
+        items: PropTypes.arrayOf(PropTypes.shape(ItemType)),
+    }),
 };
 
 export default OurWorkSection;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -25,167 +25,148 @@ import JoinUsArea from "../containers/join-us/layout-01";
 import NewsArea from "../containers/news-area/layout-01";
 
 const InfotechnoPage = ({ location, data }) => {
-    const content = normalizedData(data?.page.content || []);
-    const globalContent = normalizedData(data?.allGeneral.nodes || []);
+  const content = normalizedData(data?.page.content || []);
+  const globalContent = normalizedData(data?.allGeneral.nodes || []);
 
-    return (
-        <Layout location={location}>
-            <Seo title="Centre for Research on Engineering Software Technologies" />
-            <Header
-                data={{
-                    ...globalContent["header"],
-                    ...globalContent["menu"],
-                }}
-            />
-            <main className="site-wrapper-reveal">
-                {/* <HeroArea data={content["hero-section"]} /> */}
-                <HomeSlider data={content["slider-section"]}/>
-                <PartnerArea data={content["partner-section"]} />
-                {/* <AboutServiceWrap>
-                    <AboutArea data={content["about-section"]} />
-                    <ITServiceArea
-                        data={{
-                            ...content["service-section"],
-                            items: data.allItService.nodes,
-                        }}
-                    />
-                </AboutServiceWrap> */}
-                {/* <FunfactArea data={content["funfact-section"]} /> */}
-                {/* <CtaArea data={content["cta-section"]} /> */}
-                {/* <CaseStudyArea
-                    data={{
-                        ...content["case-study-section"],
-                        items: data.allCaseStudy.nodes,
-                    }}
-                /> */}
-                {/* <TestimonialArea data={content["testimonial-section"]} /> */}
-                <OurWorkSection />
-                <NewsArea
-                    data={{
-                        ...content["news-section"],
-                        recentNews: data.recentNews.nodes,
-                    }}
-                />
-                <BlogArea
-                    data={{
-                        ...content["blog-section"],
-                        featuredBlog: data.featuredBlogs.nodes[0],
-                        recentBlogs: data.recentBlogs.nodes,
-                    }}
-                />
-                <TwitterArea />
-                {/* <ITSolutionArea
+  return (
+    <Layout location={location}>
+      <Seo title="Centre for Research on Engineering Software Technologies" />
+      <Header
+        data={{
+          ...globalContent["header"],
+          ...globalContent["menu"],
+        }}
+      />
+      <main className="site-wrapper-reveal">
+        <HomeSlider data={content["slider-section"]} />
+        <PartnerArea data={content["partner-section"]} />
+        <OurWorkSection data={content["our-works-section"]} />
+        <NewsArea
+          data={{
+            ...content["news-section"],
+            recentNews: data.recentNews.nodes,
+          }}
+        />
+        <BlogArea
+          data={{
+            ...content["blog-section"],
+            featuredBlog: data.featuredBlogs.nodes[0],
+            recentBlogs: data.recentBlogs.nodes,
+          }}
+        />
+        <TwitterArea />
+        {/* <ITSolutionArea
                     data={{
                         ...content["feature-section"],
                         items: data.allItSolution.nodes,
                     }}
                 /> */}
-                <JoinUsArea data={content["join-us-section"]} />
-            </main>
-            <Footer data={{ ...data.site.siteMetadata }} />
-        </Layout>
-    );
+        <JoinUsArea data={content["join-us-section"]} />
+      </main>
+      <Footer data={{ ...data.site.siteMetadata }} />
+    </Layout>
+  );
 };
 
 export const query = graphql`
-    query infotechnoPageQuery {
-        allGeneral {
-            nodes {
-                section
-                ...HeaderOne
-            }
-        }
-        site {
-            ...Site
-        }
-        page(title: { eq: "infotechno" }, pageType: { eq: "frontpage" }) {
-            content {
-                ...PageContent
-            }
-        }
-        allItSolution(
-            sort: { order: DESC, fields: id }
-            filter: { is_featured: { eq: true } }
-            limit: 3
-        ) {
-            nodes {
-                ...ItSolutionTwo
-            }
-        }
-        allItService(
-            sort: { order: DESC, fields: id }
-            filter: { is_featured: { eq: false } }
-        ) {
-            nodes {
-                ...ItServiceThree
-            }
-        }
-        allCaseStudy(filter: { is_featured: { eq: true } }, limit: 4) {
-            nodes {
-                ...CaseStudyOne
-            }
-        }
-        featuredBlogs: allArticle(
-            filter: { is_featured: {eq :true} }
-            sort: { fields: postedAt___date, order: DESC }
-            limit: 1
-        ) {
-            nodes {
-                ...BlogThree
-            }
-        }
-        recentBlogs: allArticle(
-            sort: { fields: postedAt___date, order: DESC }
-            limit: 3
-        ) {
-            nodes {
-                ...BlogFour
-            }
-        }
-        recentNews: allNews(
-            sort: {fields: postedAt___date, order: DESC}
-            limit: 4
-        ) {
-            nodes {
-              ...NewsOne
-            }
-        }
+  query infotechnoPageQuery {
+    allGeneral {
+      nodes {
+        section
+        ...HeaderOne
+      }
     }
+    site {
+      ...Site
+    }
+    page(title: { eq: "infotechno" }, pageType: { eq: "frontpage" }) {
+      content {
+        ...PageContent
+      }
+    }
+    allItSolution(
+      sort: { order: DESC, fields: id }
+      filter: { is_featured: { eq: true } }
+      limit: 3
+    ) {
+      nodes {
+        ...ItSolutionTwo
+      }
+    }
+    allItService(
+      sort: { order: DESC, fields: id }
+      filter: { is_featured: { eq: false } }
+    ) {
+      nodes {
+        ...ItServiceThree
+      }
+    }
+    allCaseStudy(filter: { is_featured: { eq: true } }, limit: 4) {
+      nodes {
+        ...CaseStudyOne
+      }
+    }
+    featuredBlogs: allArticle(
+      filter: { is_featured: { eq: true } }
+      sort: { fields: postedAt___date, order: DESC }
+      limit: 1
+    ) {
+      nodes {
+        ...BlogThree
+      }
+    }
+    recentBlogs: allArticle(
+      sort: { fields: postedAt___date, order: DESC }
+      limit: 3
+    ) {
+      nodes {
+        ...BlogFour
+      }
+    }
+    recentNews: allNews(
+      sort: { fields: postedAt___date, order: DESC }
+      limit: 4
+    ) {
+      nodes {
+        ...NewsOne
+      }
+    }
+  }
 `;
 
 InfotechnoPage.propTypes = {
-    location: PropTypes.shape({}),
-    data: PropTypes.shape({
-        page: PropTypes.shape({
-            content: PropTypes.arrayOf(PropTypes.shape({})),
-        }),
-        allGeneral: PropTypes.shape({
-            nodes: PropTypes.arrayOf(PropTypes.shape({})),
-        }),
-        site: PropTypes.shape({
-            siteMetadata: PropTypes.shape({
-                socials: PropTypes.arrayOf(PropTypes.shape({})),
-            }),
-        }),
-        allItSolution: PropTypes.shape({
-            nodes: PropTypes.arrayOf(PropTypes.shape({})),
-        }),
-        allItService: PropTypes.shape({
-            nodes: PropTypes.arrayOf(PropTypes.shape({})),
-        }),
-        allCaseStudy: PropTypes.shape({
-            nodes: PropTypes.arrayOf(PropTypes.shape({})),
-        }),
-        featuredBlogs: PropTypes.shape({
-            nodes: PropTypes.arrayOf(PropTypes.shape({})),
-        }),
-        recentBlogs: PropTypes.shape({
-            nodes: PropTypes.arrayOf(PropTypes.shape({})),
-        }),
-        recentNews: PropTypes.shape({
-            nodes: PropTypes.arrayOf(PropTypes.shape({})),
-        })
+  location: PropTypes.shape({}),
+  data: PropTypes.shape({
+    page: PropTypes.shape({
+      content: PropTypes.arrayOf(PropTypes.shape({})),
     }),
+    allGeneral: PropTypes.shape({
+      nodes: PropTypes.arrayOf(PropTypes.shape({})),
+    }),
+    site: PropTypes.shape({
+      siteMetadata: PropTypes.shape({
+        socials: PropTypes.arrayOf(PropTypes.shape({})),
+      }),
+    }),
+    allItSolution: PropTypes.shape({
+      nodes: PropTypes.arrayOf(PropTypes.shape({})),
+    }),
+    allItService: PropTypes.shape({
+      nodes: PropTypes.arrayOf(PropTypes.shape({})),
+    }),
+    allCaseStudy: PropTypes.shape({
+      nodes: PropTypes.arrayOf(PropTypes.shape({})),
+    }),
+    featuredBlogs: PropTypes.shape({
+      nodes: PropTypes.arrayOf(PropTypes.shape({})),
+    }),
+    recentBlogs: PropTypes.shape({
+      nodes: PropTypes.arrayOf(PropTypes.shape({})),
+    }),
+    recentNews: PropTypes.shape({
+      nodes: PropTypes.arrayOf(PropTypes.shape({})),
+    }),
+  }),
 };
 
 export default InfotechnoPage;
