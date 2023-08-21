e858354da43b824a71f62f612b81e7fee53adca9
CalvinWang <wyf199869@gmail.com>
2023-08-21 19:17:43 +0930

    update 20 popup video

---
+++ src/containers/elements/popup-video/imageImports.jsx
@@ -0,0 +1,47 @@
+// imageImports.js
+import image1 from "@data/images/box-image/video-image-01.jpg";
+import image2 from "@data/images/box-image/video-image-02.jpg";
+import image3 from "@data/images/box-image/video-image-03.jpg";
+import image4 from "@data/images/box-image/video-image-04.jpg";
+import image5 from "@data/images/box-image/video-image-05.jpg";
+import image6 from "@data/images/box-image/video-image-06.jpg";
+import image7 from "@data/images/box-image/video-image-07.jpg";
+import image8 from "@data/images/box-image/video-image-08.jpg";
+import image9 from "@data/images/box-image/video-image-09.jpg";
+import image10 from "@data/images/box-image/video-image-10.jpg";
+import image11 from "@data/images/box-image/video-image-11.jpg";
+import image12 from "@data/images/box-image/video-image-12.jpg";
+import image13 from "@data/images/box-image/video-image-13.jpg";
+import image14 from "@data/images/box-image/video-image-14.jpg";
+import image15 from "@data/images/box-image/video-image-15.jpg";
+import image16 from "@data/images/box-image/video-image-16.jpg";
+import image17 from "@data/images/box-image/video-image-17.jpg";
+import image18 from "@data/images/box-image/video-image-18.jpg";
+import image19 from "@data/images/box-image/video-image-19.jpg";
+import image20 from "@data/images/box-image/video-image-20.jpg";
+// ... 其他图片
+
+export default {
+    image1,
+    image2,
+    image3,
+    image4,
+    image4,
+    image5,
+    image6,
+    image7,
+    image8,
+    image9,
+    image10,
+    image11,
+    image12,
+    image13,
+    image14,
+    image15,
+    image16,
+    image17,
+    image18,
+    image19,
+    image20,
+    // ... 其他图片
+};
--- src/containers/elements/popup-video/index.jsx
+++ src/containers/elements/popup-video/index.jsx
@@ -1,128 +1,132 @@
 import React from "react";
 import { Container, Row, Col } from "@ui/wrapper";
-import PopupVideoOne from "@ui/popup-video/layout-01";
 import PopupVideoTwo from "@ui/popup-video/layout-02";
-import ImageOne from "@data/images/box-image/video-image-01.jpg";
 import { SectionWrap } from "./style";
+import images from './imageImports';
+
+const videoData = [
+    { 
+        link: "https://www.youtube.com/watch?v=1S5drvhJaKY", 
+        title: "GitHub SOCRATES Training",
+        image: images.image1
+    },
+    { 
+        link: "https://www.youtube.com/watch?v=LY3nXQjMJmo", 
+        title: "Developers' discussions about security challenges of different programming languages",
+        image: images.image2
+    },
+    { 
+        link: "https://www.youtube.com/watch?v=Co_PqcG-DXg", 
+        title: "LineVD: Statement-level Vulnerability Detection using Graph Neural Networks",
+        image: images.image3
+    },
+    { 
+        link: "https://www.youtube.com/watch?v=LPeMjo8dUIw", 
+        title: "116-TECH-CROFT",
+        image: images.image4
+    },
+    { 
+        link: "https://www.youtube.com/watch?v=C97fAd93BSY", 
+        title: "On the Use of Fine-Grained Vulnerable Code Statements for Software Vulnerability Assessment Models",
+        image: images.image5
+    },
+    { 
+        link: "https://www.youtube.com/watch?v=zctWy4zdqhY", 
+        title: "Analytics over Encrypted Traffic and Defences",
+        image: images.image6
+    },
+    { 
+        link: "https://www.youtube.com/watch?v=zg-GwJjYdEw", 
+        title: "CyberSecurity4AI & AI4CyberSecurity",
+        image: images.image7
+    },
+    { 
+        link: "https://www.youtube.com/watch?v=dCDQp-3idC4", 
+        title: "Well Begun is Half Done: An Empirical Study of Exploitability & Impact of Base-Image Vulnerabilities",
+        image: images.image8
+    },
+    { 
+        link: "https://www.youtube.com/watch?v=DAgOliuFEcc", 
+        title: "KGSecConfig: A Knowledge Graph Based Approach for Secured Container Orchestrator Configuration",
+        image: images.image9
+    },
+    { 
+        link: "https://www.youtube.com/watch?v=3_XysfuPF-A", 
+        title: "An Investigation into Inconsistency of Software Vulnerability Severity across Data Sources",
+        image: images.image10
+    },
+    { 
+        link: "https://www.youtube.com/watch?v=WpDx2QIFP7c", 
+        title: "Developers’ discussions about security challenges of different programming languages",
+        image: images.image11
+    },
+    { 
+        link: "https://www.youtube.com/watch?v=pveQo19ZPeA", 
+        title: "Falling for Phishing: An Empirical Investigation into People's Email Response Behaviors",
+        image: images.image12
+    },
+    { 
+        link: "https://www.youtube.com/watch?v=YhuZvtkfPPo", 
+        title: "DeepCVA: Automated Commit-level Vulnerability Assessment with Deep Multi-task Learning",
+        image: images.image13
+    },
+    { 
+        link: "https://www.youtube.com/watch?v=Ajc4kWLMBZQ", 
+        title: "An Empirical Study of Rule-Based & Learning-Based Approaches for Static Application Security Testing",
+        image: images.image14
+    },
+    { 
+        link: "https://www.youtube.com/watch?v=GN5sTddZmy4", 
+        title: "Taxonomy of Edge Blockchain Network Designs",
+        image: images.image15
+    },
+    { 
+        link: "https://www.youtube.com/watch?v=bo7I1D6hd5g", 
+        title: "A Grounded Theory of the Role of Coordination in Software Security Patch Management",
+        image: images.image16
+    },
+    { 
+        link: "https://www.youtube.com/watch?v=5MtABLSxeRo", 
+        title: "QuickAdapt: Scalable Adaptation for Big Data Cyber Security Analytics",
+        image: images.image17
+    },
+    { 
+        link: "https://www.youtube.com/watch?v=k2y4lcm3fLo", 
+        title: "Evaluation of Big Data Systems in Edge and Cloud Environments",
+        image: images.image18
+    },
+    { 
+        link: "https://www.youtube.com/watch?v=-Pihuj1ebG8", 
+        title: "Mining Questions Asked about Continuous Software Engineering: A Case Study of Stack Overflow",
+        image: images.image19
+    },
+    { 
+        link: "https://www.youtube.com/watch?v=SgAmX6CFb0o", 
+        title: "Security Awareness of End Users of Mobile Health Applications: An Empirical Study",
+        image: images.image20
+    },
+];
 
 const PopupVideoArea = () => {
     return (
         <SectionWrap>
             <Container>
-                <Row style={{ marginBottom: '40px' }}>
-                    <Col md={3} className="popup-one">
-                        <PopupVideoTwo
-                            image={{ src: ImageOne }}
-                            video_button={{
-                                link: "https://www.youtube.com/watch?v=1S5drvhJaKY",
-                            }}
-                        />
-                        <div>GitHub SOCRATES Training</div>
-                    </Col>
-                    <Col md={3} className="popup-one">
-                        <PopupVideoOne
-                            image={{ src: ImageOne }}
-                            video_button={{
-                                link: "https://www.youtube.com/watch?v=LY3nXQjMJmo",
-                            }}
-                        />
-                        <div>Developers' discussions about security challenges of different programming languages</div>
-                    </Col>
-                    <Col md={3} className="popup-one">
-                        <PopupVideoOne
-                            image={{ src: ImageOne }}
-                            video_button={{
-                                link: "https://www.youtube.com/watch?v=Co_PqcG-DXg",
-                            }}
-                        />
-                        <div>LineVD: Statement-level Vulnerability Detection using Graph Neural Networks</div>
-                    </Col>
-                    <Col md={3} className="popup-one">
-                        <PopupVideoOne
-                            image={{ src: ImageOne }}
-                            video_button={{
-                                link: "https://www.youtube.com/watch?v=LPeMjo8dUIw",
-                            }}
-                        />
-                    <div>116-TECH-CROFT</div>
-                    </Col>
-                </Row>
-                <Row style={{ marginBottom: '40px' }}>
-                    <Col md={3} className="popup-one">
-                        <PopupVideoOne
-                            image={{ src: ImageOne }}
-                            video_button={{
-                                link: "https://www.youtube.com/watch?v=C97fAd93BSY",
-                            }}
-                        />
-                    <div>On the Use of Fine-Grained Vulnerable Code Statements for Software Vulnerability Assessment Models</div>
-                    </Col>
-                    <Col md={3} className="popup-one">
-                        <PopupVideoOne
-                            image={{ src: ImageOne }}
-                            video_button={{
-                                link: "https://www.youtube.com/watch?v=zctWy4zdqhY",
-                            }}
-                        />
-                    <div>An Investigation into Inconsistency of Software Vulnerability Severity across Data Sources</div>
-                    </Col>
-                    <Col md={3} className="popup-one">
-                        <PopupVideoOne
-                            image={{ src: ImageOne }}
-                            video_button={{
-                                link: "https://www.youtube.com/watch?v=zg-GwJjYdEw",
-                            }}
-                        />
-                    <div>CyberSecurity4AI & AI4CyberSecurity</div>
-                    </Col>
-                    <Col md={3} className="popup-one">
-                        <PopupVideoOne
-                            image={{ src: ImageOne }}
-                            video_button={{
-                                link: "https://www.youtube.com/watch?v=dCDQp-3idC4",
-                            }}
-                        />
-                    <div>Well Begun is Half Done: An Empirical Study of Exploitability & Impact of Base-Image Vulnerabilities</div>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md={3} className="popup-one">
-                        <PopupVideoOne
-                            image={{ src: ImageOne }}
-                            video_button={{
-                                link: "https://www.youtube.com/watch?v=5MtABLSxeRo",
-                            }}
-                        />
-                    </Col>
-                    <Col md={3} className="popup-one">
-                        <PopupVideoOne
-                            image={{ src: ImageOne }}
-                            video_button={{
-                                link: "https://www.youtube.com/watch?v=3_XysfuPF-A",
-                            }}
-                        />
-                    </Col>
-                    <Col md={3} className="popup-one">
-                        <PopupVideoOne
-                            image={{ src: ImageOne }}
-                            video_button={{
-                                link: "https://www.youtube.com/watch?v=WpDx2QIFP7c",
-                            }}
-                        />
-                    </Col>
-                    <Col md={3} className="popup-one">
-                        <PopupVideoOne
-                            image={{ src: ImageOne }}
-                            video_button={{
-                                link: "https://www.youtube.com/watch?v=pveQo19ZPeA",
-                            }}
-                        />
-                    </Col>
-                </Row>
+                {Array(Math.ceil(videoData.length / 4)).fill().map((_, rowIndex) => (
+                    <Row key={rowIndex} style={{ marginBottom: '40px' }}>
+                        {videoData.slice(rowIndex * 4, (rowIndex + 1) * 4).map((video, colIndex) => (
+                            <Col key={colIndex} md={3} className="popup-one">
+                                <PopupVideoTwo
+                                    image={{ src: video.image }}
+                                    video_button={{ link: video.link }}
+                                />
+                                <div>{video.title}</div>
+                            </Col>
+                        ))}
+                    </Row>
+                ))}
             </Container>
         </SectionWrap>
     );
 };
 
 export default PopupVideoArea;
-
