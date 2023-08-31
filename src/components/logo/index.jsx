f7efbac95e85051a52b72ba34921083ed42ccfa6
CalvinWang <wyf199869@gmail.com>
2023-08-31 11:29:16 +0930

    Adelaide Uni logo has been added

--- src/components/logo/index.jsx
+++ src/components/logo/index.jsx
@@ -5,36 +5,43 @@ import { StaticImage } from "gatsby-plugin-image";
 import { LogoWrapper } from "./style";
 
 const Logo = ({ className, variant, ...restProps }) => {
-    return (
-        <LogoWrapper className={className} $variant={variant} {...restProps}>
-            <Link to="/">
-                <StaticImage
-                    src="../../assets/images/logo/light-logo.png"
-                    className="img-fluid static-img light-logo"
-                    alt="logo"
-                    width={160}
-                    height={48}
-                />
-                <StaticImage
-                    src="../../assets/images/logo/dark-logo.png"
-                    className="img-fluid static-img dark-logo"
-                    alt="logo"
-                    width={160}
-                    height={48}
-                />
-            </Link>
-        </LogoWrapper>
-    );
+  return (
+    <LogoWrapper className={className} $variant={variant} {...restProps}>
+      <Link to="/">
+        <StaticImage
+          src="../../assets/images/logo/light-logo.png"
+          className="img-fluid static-img light-logo"
+          alt="logo"
+          width={160}
+          height={48}
+        />
+        <StaticImage
+          src="../../assets/images/logo/dark-logo.png"
+          className="img-fluid static-img dark-logo"
+          alt="logo"
+          width={160}
+          height={48}
+        />
+        <StaticImage
+          src="../../assets/images/logo/adelaide.png"
+          className="img-fluid static-img adelaide-logo"
+          alt="Adelaide Uni logo"
+          width={140}
+          height={40}
+        />
+      </Link>
+    </LogoWrapper>
+  );
 };
 
 Logo.propTypes = {
-    className: PropTypes.string,
-    variant: PropTypes.oneOf(["white", "dark"]),
+  className: PropTypes.string,
+  variant: PropTypes.oneOf(["white", "dark"]),
 };
 
 Logo.defaultProps = {
-    justifyContent: "flex-start",
-    variant: "dark",
+  justifyContent: "flex-start",
+  variant: "dark",
 };
 
 export default Logo;
--- src/components/logo/style.jsx
+++ src/components/logo/style.jsx
@@ -1,33 +1,38 @@
 import styled, { css, space, flexbox } from "@styled";
 
 export const LogoWrapper = styled.div`
-    display: flex;
-    ${flexbox};
-    ${space};
-    a {
-        img {
-            width: 160px;
-        }
-    }
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-    ${({ $variant }) =>
-        $variant === "white" &&
-        css`
-            .dark-logo {
-                display: none !important;
-            }
-            .light-logo {
-                display: inherit;
-            }
-        `}
-    ${({ $variant }) =>
-        $variant === "dark" &&
-        css`
-            .dark-logo {
-                display: inherit;
-            }
-            .light-logo {
-                display: none !important;
-            }
-        `}
+  a {
+    display: flex;
+    align-items: center;
+
+    .static-img {
+      margin-right: 10px;
+      max-width: 100%; /* Limit the maximum width of the image */
+    }
+  }
+
+  ${({ $variant }) =>
+    $variant === "white" &&
+    css`
+      .dark-logo {
+        display: none !important;
+      }
+      .light-logo {
+        display: inherit;
+      }
+    `}
+  ${({ $variant }) =>
+    $variant === "dark" &&
+    css`
+      .dark-logo {
+        display: inherit;
+      }
+      .light-logo {
+        display: none !important;
+      }
+    `}
 `;
