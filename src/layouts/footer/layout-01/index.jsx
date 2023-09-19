892c51a5c1bd37522c1ee78b46707d6905734884
CalvinWang <wyf199869@gmail.com>
2023-09-19 12:19:35 +0930

    hide support footer, hide project slide bar,

--- src/layouts/footer/layout-01/index.jsx
+++ src/layouts/footer/layout-01/index.jsx
@@ -10,62 +10,60 @@ import Heading from "@ui/heading";
 import Social, { SocialLink } from "@ui/social";
 import { SocialType } from "@utils/types";
 import {
-    FooterWrap,
-    FooterTop,
-    FooterWidget,
-    LogoWidget,
-    TextWidget,
-    FooterWidgetList,
-    FooterBottom,
+  FooterWrap,
+  FooterTop,
+  FooterWidget,
+  LogoWidget,
+  TextWidget,
+  FooterWidgetList,
+  FooterBottom,
 } from "./style";
 
 const Footer = ({ data }) => {
-    return (
-        <FooterWrap>
-            <FooterTop>
-                <Container>
-                    <Row>
-                        <Col lg={4} sm={6}>
-                            <FooterWidget mb={["31px", null, null, 0]}>
-                                <LogoWidget>
-                                    <StaticImage
-                                        src="../../../assets/images/logo/dark-logo.png"
-                                        alt="logo"
-                                        width={160}
-                                        height={48}
-                                    />
-                                </LogoWidget>
-                                {data?.contact && (
-                                    <TextWidget>
-                                        {data.contact?.address && (
-                                            <Text mb="10px">
-                                                {data.contact.address}
-                                            </Text>
-                                        )}
-                                        {data.contact?.email && (
-                                            <Text mb="10px">
-                                                <Anchor
-                                                    path={`mailto:${data.contact.email}`}
-                                                    color="text"
-                                                    $hover={{ layout: 2 }}
-                                                >
-                                                    {data.contact.email}
-                                                </Anchor>
-                                            </Text>
-                                        )}
-                                        {data.contact?.phone && (
-                                            <Text mb="10px">
-                                                <Anchor
-                                                    path={`tel:${data.contact.phone}`}
-                                                    fontWeight="800"
-                                                    color="heading"
-                                                    $hover={{ layout: 2 }}
-                                                >
-                                                    {data.contact.phone}
-                                                </Anchor>
-                                            </Text>
-                                        )}
-                                        {/* {data.contact?.website && (
+  return (
+    <FooterWrap>
+      <FooterTop>
+        <Container>
+          <Row>
+            <Col lg={4} sm={6}>
+              <FooterWidget mb={["31px", null, null, 0]}>
+                <LogoWidget>
+                  <StaticImage
+                    src="../../../assets/images/logo/dark-logo.png"
+                    alt="logo"
+                    width={160}
+                    height={48}
+                  />
+                </LogoWidget>
+                {data?.contact && (
+                  <TextWidget>
+                    {data.contact?.address && (
+                      <Text mb="10px">{data.contact.address}</Text>
+                    )}
+                    {data.contact?.email && (
+                      <Text mb="10px">
+                        <Anchor
+                          path={`mailto:${data.contact.email}`}
+                          color="text"
+                          $hover={{ layout: 2 }}
+                        >
+                          {data.contact.email}
+                        </Anchor>
+                      </Text>
+                    )}
+                    {data.contact?.phone && (
+                      <Text mb="10px">
+                        <Anchor
+                          path={`tel:${data.contact.phone}`}
+                          fontWeight="800"
+                          color="heading"
+                          $hover={{ layout: 2 }}
+                        >
+                          {data.contact.phone}
+                        </Anchor>
+                      </Text>
+                    )}
+                    {/* {data.contact?.website && (
                                             <Text mb="10px">
                                                 <Anchor
                                                     path={data.contact.website}
@@ -75,53 +73,49 @@ const Footer = ({ data }) => {
                                                 </Anchor>
                                             </Text>
                                         )} */}
-                                    </TextWidget>
-                                )}
-                            </FooterWidget>
-                        </Col>
-                        <Col lg={3} md={6} sm={6}>
-                            <FooterWidget mb={["31px", null, null, 0]}>
-                                <Heading as="h6" mt="-3px" mb="20px">
-                                    Clusters
-                                </Heading>
-                                <FooterWidgetList>
-                                    <li>
-                                        <Anchor
-                                            path="/"
-                                            color="text"
-                                            $hover={{ layout: 2 }}
-                                        >
-                                            Big Data
-                                        </Anchor>
-                                    </li>
-                                    <li>
-                                        <Anchor
-                                            path="https://ssi.crest-centre.net/"
-                                            color="text"
-                                            $hover={{ layout: 2 }}
-                                        >
-                                            Software Security Intelligence
-                                        </Anchor>
-                                    </li>
-                                    <li>
-                                        <Anchor
-                                            path="http://socio-technical.crest-centre.net/"
-                                            color="text"
-                                            $hover={{ layout: 2 }}
-                                        >
-                                            Socio Technical
-                                        </Anchor>
-                                    </li>
-                                    <li>
-                                        <Anchor
-                                            path="https://dlt.crest-centre.net/"
-                                            color="text"
-                                            $hover={{ layout: 2 }}
-                                        >
-                                            Distributed Ledger Technology
-                                        </Anchor>
-                                    </li>
-                                    {/* <li>
+                  </TextWidget>
+                )}
+              </FooterWidget>
+            </Col>
+            <Col lg={3} md={6} sm={6}>
+              <FooterWidget mb={["31px", null, null, 0]}>
+                <Heading as="h6" mt="-3px" mb="20px">
+                  Clusters
+                </Heading>
+                <FooterWidgetList>
+                  <li>
+                    <Anchor path="/" color="text" $hover={{ layout: 2 }}>
+                      Big Data
+                    </Anchor>
+                  </li>
+                  <li>
+                    <Anchor
+                      path="https://ssi.crest-centre.net/"
+                      color="text"
+                      $hover={{ layout: 2 }}
+                    >
+                      Software Security Intelligence
+                    </Anchor>
+                  </li>
+                  <li>
+                    <Anchor
+                      path="http://socio-technical.crest-centre.net/"
+                      color="text"
+                      $hover={{ layout: 2 }}
+                    >
+                      Socio Technical
+                    </Anchor>
+                  </li>
+                  <li>
+                    <Anchor
+                      path="https://dlt.crest-centre.net/"
+                      color="text"
+                      $hover={{ layout: 2 }}
+                    >
+                      Distributed Ledger Technology
+                    </Anchor>
+                  </li>
+                  {/* <li>
                                         <Anchor
                                             path="/"
                                             color="text"
@@ -130,10 +124,10 @@ const Footer = ({ data }) => {
                                             Cyber Security
                                         </Anchor>
                                     </li> */}
-                                </FooterWidgetList>
-                            </FooterWidget>
-                        </Col>
-                        {/* <Col lg={2} md={4} sm={6}>
+                </FooterWidgetList>
+              </FooterWidget>
+            </Col>
+            {/* <Col lg={2} md={4} sm={6}>
                             <FooterWidget mb={["27px", null, 0]}>
                                 <Heading as="h6" mt="-3px" mb="20px">
                                     Quick links
@@ -178,61 +172,8 @@ const Footer = ({ data }) => {
                                 </FooterWidgetList>
                             </FooterWidget>
                         </Col> */}
-                        <Col lg={3} md={6} sm={6}>
-                            <FooterWidget>
-                                <Heading as="h6" mt="-3px" mb="20px">
-                                    Support
-                                </Heading>
-                                <FooterWidgetList>
-                                    <li>
-                                        <Anchor
-                                            path="/"
-                                            color="text"
-                                            $hover={{ layout: 2 }}
-                                        >
-                                            Forum Support
-                                        </Anchor>
-                                    </li>
-                                    <li>
-                                        <Anchor
-                                            path="/"
-                                            color="text"
-                                            $hover={{ layout: 2 }}
-                                        >
-                                            Help &amp; FAQ
-                                        </Anchor>
-                                    </li>
-                                    <li>
-                                        <Anchor
-                                            path="/"
-                                            color="text"
-                                            $hover={{ layout: 2 }}
-                                        >
-                                            Contact Us
-                                        </Anchor>
-                                    </li>
-                                    <li>
-                                        <Anchor
-                                            path="/"
-                                            color="text"
-                                            $hover={{ layout: 2 }}
-                                        >
-                                            Pricing and plans
-                                        </Anchor>
-                                    </li>
-                                    <li>
-                                        <Anchor
-                                            path="/"
-                                            color="text"
-                                            $hover={{ layout: 2 }}
-                                        >
-                                            Cookies Policy
-                                        </Anchor>
-                                    </li>
-                                </FooterWidgetList>
-                            </FooterWidget>
-                        </Col>
-                        {/* <Col lg={2} md={4} sm={6}>
+
+            {/* <Col lg={2} md={4} sm={6}>
                             <FooterWidget mt={["34px", null, "50px"]}>
                                 <FooterWidgetList>
                                     <li>
@@ -258,67 +199,62 @@ const Footer = ({ data }) => {
                                 </FooterWidgetList>
                             </FooterWidget>
                         </Col> */}
-                    </Row>
-                </Container>
-            </FooterTop>
-            <FooterBottom>
-                <Container>
-                    <Row alignItems="center">
-                        <Col md={6} textAlign={["center", null, "left"]}>
-                            {data?.copyright && (
-                                <Text pb={["15px", 0]}>
-                                    &copy; {new Date().getFullYear()}{" "}
-                                    <span
-                                        dangerouslySetInnerHTML={{
-                                            __html: data.copyright,
-                                        }}
-                                    />
-                                </Text>
-                            )}
-                        </Col>
-                        <Col md={6} textAlign={["center", null, "right"]}>
-                            {data?.socials && (
-                                <Social
-                                    space="16px"
-                                    tooltip={true}
-                                    shape="rounded"
-                                    variant="outlined"
-                                >
-                                    {data.socials.map((social) => (
-                                        <SocialLink
-                                            key={social.id}
-                                            path={social.link}
-                                            title={social.title}
-                                        >
-                                            <i
-                                                className={cn(
-                                                    social.icon,
-                                                    "link-icon"
-                                                )}
-                                            ></i>
-                                        </SocialLink>
-                                    ))}
-                                </Social>
-                            )}
-                        </Col>
-                    </Row>
-                </Container>
-            </FooterBottom>
-        </FooterWrap>
-    );
+          </Row>
+        </Container>
+      </FooterTop>
+      <FooterBottom>
+        <Container>
+          <Row alignItems="center">
+            <Col md={6} textAlign={["center", null, "left"]}>
+              {data?.copyright && (
+                <Text pb={["15px", 0]}>
+                  &copy; {new Date().getFullYear()}{" "}
+                  <span
+                    dangerouslySetInnerHTML={{
+                      __html: data.copyright,
+                    }}
+                  />
+                </Text>
+              )}
+            </Col>
+            <Col md={6} textAlign={["center", null, "right"]}>
+              {data?.socials && (
+                <Social
+                  space="16px"
+                  tooltip={true}
+                  shape="rounded"
+                  variant="outlined"
+                >
+                  {data.socials.map((social) => (
+                    <SocialLink
+                      key={social.id}
+                      path={social.link}
+                      title={social.title}
+                    >
+                      <i className={cn(social.icon, "link-icon")}></i>
+                    </SocialLink>
+                  ))}
+                </Social>
+              )}
+            </Col>
+          </Row>
+        </Container>
+      </FooterBottom>
+    </FooterWrap>
+  );
 };
 
 Footer.propTypes = {
-    data: PropTypes.shape({
-        socials: PropTypes.arrayOf(PropTypes.shape(SocialType)),
-        copyright: PropTypes.string,
-        contact: PropTypes.shape({
-            phone: PropTypes.string,
-            email: PropTypes.string,
-            address: PropTypes.string,
-            website: PropTypes.string,
-        }),
+  data: PropTypes.shape({
+    socials: PropTypes.arrayOf(PropTypes.shape(SocialType)),
+    copyright: PropTypes.string,
+    contact: PropTypes.shape({
+      phone: PropTypes.string,
+      email: PropTypes.string,
+      address: PropTypes.string,
+      website: PropTypes.string,
     }),
+  }),
 };
 
 export default Footer;
